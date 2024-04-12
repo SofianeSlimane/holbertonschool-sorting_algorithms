87df3044e9d2b313016d7a431af379d7641722b4
Goliath33 <gauliath@msn.com>
2024-04-12 13:33:46 +0000

    add file *.c for exercices

--- sort.h
+++ sort.h
@@ -1,8 +1,5 @@
 #ifndef SORT_H
 #define SORT_H
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
 
 /**
  * struct listint_s - Doubly linked list node
@@ -11,6 +8,7 @@ void print_list(const listint_t *list);
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
     const int n;
@@ -19,16 +17,12 @@ typedef struct listint_s
 } listint_t;
 
 
-
-
-
-
-
-
-
-
-
-
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 #endif
