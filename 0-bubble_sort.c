ce4fb7174eb383172fca01696cea935e32a36792
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-12 22:45:33 +0000

    Added O-file, corrected betty for sort.h and task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,16 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array
+ * @size: size of array
+ */
 
 
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
 	size_t i, j;
+
 	i = 0;
 	j = 0;
 	while (j < size)
@@ -13,20 +18,15 @@ void bubble_sort(int *array, size_t size)
 		i = 0;
 	while (i < size - 1)
 	{
-	 		
-	
 		if (array[i + 1] != '\0' && array[i] > array[i + 1])
-	   	{	
-		  tmp = array[i];
-		  array[i] = array[i + 1];
-		  array[i + 1] = tmp;
-		  print_array(array, size);
+		{
+			tmp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = tmp;
+			print_array(array, size);
 		}
-		
-	   	i++;
+		i++;
 		}
 	j++;
 	}
-
-	   
-}	
+}
