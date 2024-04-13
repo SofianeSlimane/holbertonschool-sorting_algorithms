2865b2b83638134fb3bbb48c857740eac90adadd
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-13 17:20:12 +0000

    Saving work for task 1, still working on loops

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,18 +9,18 @@ void insertion_sort_list(listint_t **list)
 	while (ptr->next != NULL)
 	{
 		
-		if (ptr->n > ptr->next->n)
+		if (ptr->prev != NULL && ptr->n > ptr->next->n)
 		{
 			ptr->next->prev = ptr->prev;
 			ptr->prev->next = ptr->next;
+
 			ptr->prev = ptr->next;
 			ptr->next = ptr->next->next;
 			ptr->next->prev = ptr;
+			ptr->prev->next = ptr;
 			print_list(*list);
 		}
-		else
-		{
-			ptr = ptr->next;
-		}
+		ptr = ptr->next;
 	}
+	
 }	
