e0111123ff8019da90acc37057fb6eab347dafe2
SofianeSlimane <sofianeslimane24@gmail.com>
2026-05-18 20:43:27 +0200

    Betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,51 +9,41 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *p, *tmp;
 	int i = 0, j = 0;
+
 	if (list == NULL || (*list)->next == NULL)
 		return;
-	
-
 	p = *list;
 	while (p->next)
 	{
-		
 		if (p->n > p->next->n)
 		{
 			tmp = p;
 			while (p->n > p->next->n)
-			{ 				
+			{
 				p->next->prev = p->prev;
 				if (p->prev)
-				{
 					p->prev->next = p->next;
-				}
-				
-				p->prev= p->next;
-				
+				p->prev = p->next;
 				p->next = p->next->next;
-				
 				p->prev->next = p;
-				
 				if (p->next)
 					p->next->prev = p;
-
 				if (p->prev->prev)
-				{
 					p = p->prev->prev;
-				}
-				else if (p->prev){
+				else if (p->prev)
+				{
 					p = p->prev;
 					*list = p;
 				}
-				
 				print_list(*list);
 				i++;
 			}
 			p = tmp;
 		}
-		else {
+		else
+		{
 			p = p->next;
 		}
 		j++;
-	}	
+	}
 }
